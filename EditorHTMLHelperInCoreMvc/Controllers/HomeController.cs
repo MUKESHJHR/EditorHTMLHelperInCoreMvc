d7aa4bc8d1fd431409ec02055ace5408afc29d27
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-22 03:44:02 +0530

    Add project files.

---
+++ EditorHTMLHelperInCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using EditorHTMLHelperInCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EditorHTMLHelperInCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                EmployeeId=101,
+                Name="Mukesh Kumar",
+                Gender=Gender.Male,
+                Age=34,
+                IsNewlyAdded=true,
+                DOB=Convert.ToDateTime("10-06-1989")
+
+            };
+            return View(emp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EditorHTMLHelperInCoreMvc/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace EditorHTMLHelperInCoreMvc.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? Name  { get; set; }
+        public Gender Gender { get; set; }
+
+        public int Age { get; set; }
+        public bool IsNewlyAdded { get; set; }
+        public DateTime? DOB { get; set; }
+
+    }
+    public enum Gender
+    {
+        Male, 
+        Female
+    }
+}
